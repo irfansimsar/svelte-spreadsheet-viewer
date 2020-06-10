ea9a2ec0fac9dccd64331fe402d1492bcafb15e9
irfan simsar <isimsar@blutv.com>
2020-06-11 00:15:38 +0300

    Add file input to upload spreadsheet files

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,25 @@
 <script>
 	export let name;
+	export let files;
+
+	$: if (files.length) {
+		console.log('*files', files)
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>{name}</h1>
+	{#if !files.length}
+		<div class="drop-area">
+			Upload your spreadsheet file!
+			<input
+				type="file"
+				accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+				bind:files>
+		</div>
+	{:else}
+		<p><strong>{files[0].name}</strong> is uploaded and its size is <strong>{files[0].size}</strong> bytes</p>
+	{/if}
 </main>
 
 <style>
@@ -20,6 +35,17 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	.drop-area {
+		border: 2px dashed #ddd;
+		display: inline-block;
+		padding: 50px;
+	}
+
+	.drop-area input {
+		display: block;
+		margin-top: 20px;
 	}
 
 	@media (min-width: 640px) {
