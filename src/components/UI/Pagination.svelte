e4bf78a845fef802f4268b32b394b0cca87fc046
irfan simsar <isimsar@blutv.com>
2020-06-13 16:31:40 +0300

    Add Pagination UI component

---
+++ src/components/UI/Pagination.svelte
@@ -0,0 +1,47 @@
+<script>
+	export let current = 1;
+	export let pageSize = 20;
+	export let total = 20;
+	export let onChange = () => {};
+
+	const pages = [...Array(total / pageSize).keys()];
+	const handleOnChange = (page) => {
+		current = page;
+		onChange(page);
+	};
+</script>
+
+<div class="pagination">
+  {#each pages as page}
+    <button
+      class:current={current === page + 1}
+      on:click={() => handleOnChange(page + 1)}>
+        {page + 1}
+      </button>
+  {/each}
+</div>
+
+<style>
+  .pagination {
+    margin: 20px 0;
+  }
+
+  .pagination button {
+    width: 40px;
+    height: 40px;
+    transition: var(--transition-all);
+    margin: 0 2px;
+  }
+
+  .pagination button:hover,
+  .pagination button.current {
+    background: var(--color-primary);
+    border-color: var(--color-primary);
+    color: #fff;
+    opacity: .8;
+  }
+
+  .pagination button.current {
+    opacity: 1;
+  }
+</style>
--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,7 +1,9 @@
 import Button from './Button.svelte';
 import Image from './Image.svelte';
+import Pagination from './Pagination.svelte';
 
 export {
 	Button,
-	Image
+	Image,
+	Pagination,
 };
