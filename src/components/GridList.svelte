79e223811fc40e2dba6a2a29e5424cdb9796a777
irfan simsar <isimsar@gmail.com>
2020-06-18 01:42:07 +0300

    Update GridList component with grid/list switch toggle support

--- src/components/GridList.svelte
+++ src/components/GridList.svelte
@@ -4,6 +4,7 @@
   let listItems = $items || [];
   const pageSize = 10;
   let currentPage = 1;
+  let isGrid = false;
 
   function isImage(key) {
     const column = $columns.find(col => col.id === key);
@@ -20,6 +21,11 @@
     return column && column.isVisible;
   }
 
+  function onToggle(type) {
+    isGrid = type === 'grid';
+    console.log('*isGrid', isGrid);
+  }
+
   function onChange(page) {
     currentPage = page;
     const skip = (currentPage - 1) * pageSize;
@@ -28,39 +34,49 @@
   }
 
   columns.subscribe(() => {
-    const skip = (currentPage - 1) * pageSize;
-    const limit = skip + pageSize;
-    listItems = [...$items.slice(skip, limit)];
-    listItems.forEach(item => {
-      Object.keys(item).forEach(key => {
-        if (!isVisible(key)) delete item[key];
-      });
-    });
+    onChange(currentPage);
   });
 </script>
 
-<div class="grid-list">
-  <div class="row">
-    {#each Object.keys(listItems[0]) as key}
-      <div><strong>{key}</strong></div>
-    {/each}
+<div class={`grid-list ${isGrid ? 'grid' : 'list'}`}>
+  <div class="toggle-wrapper">
+    <button
+      on:click={() => onToggle('list')}
+      class="btn-list"
+    >list</button>
+    <button
+      on:click={() => onToggle('grid')}
+      class="btn-grid"
+    >grid</button>
   </div>
+
+  {#if !isGrid}
+    <div class="row">
+      {#each Object.keys(listItems[0]) as key}
+        {#if isVisible(key)}
+          <div><strong>{key}</strong></div>
+        {/if}
+      {/each}
+    </div>
+  {/if}
 
   {#each listItems as item}
     <div class="row">
       {#each Object.keys(item) as key}
-        <div>
-          {#if isImage(key)}
-            <a href={item[key]} target="_blank">
-              <Image src={item[key]} />
-            </a>
-          {:else}
-            {item[key]}
-          {/if}
-          {#if isEditable(key)}
-            <input bind:value={item[key]} />
-          {/if}
-        </div>
+        {#if isVisible(key)}
+          <div>
+            {#if isImage(key)}
+              <a href={item[key]} target="_blank">
+                <Image src={item[key]} />
+              </a>
+            {:else}
+              {item[key]}
+            {/if}
+            {#if isEditable(key)}
+              <input bind:value={item[key]} />
+            {/if}
+          </div>
+        {/if}
       {/each}
     </div>
   {/each}
@@ -77,6 +93,20 @@
     display: table;
   }
 
+  .grid-list .toggle-wrapper {
+    display: flex;
+  }
+
+  .grid-list .toggle-wrapper button {
+    border: none;
+    border-radius: 0;
+  }
+
+  .grid-list .btn-list {
+    background: var(--color-primary);
+    color: var(--color-white);
+  }
+  
   .row {
     margin-bottom: 3px;
     padding: 10px 20px;
@@ -102,5 +132,31 @@
 
   .row input {
     margin-top: 10px;
+  }
+
+  .grid-list.grid {
+    display: block;
+    text-align: left;
+  }
+
+  .grid-list.grid .btn-grid {
+    background: var(--color-primary);
+    color: var(--color-white);
+  }
+
+  .grid-list.grid .btn-list {
+    background-color: var(--color-text-light);
+    color: var(--color-text);
+  }
+
+  .grid-list.grid .row {
+    width: 200px;
+    display: inline-block;
+    margin: 20px;
+    vertical-align: top;
+  }
+
+  .grid-list.grid .row div {
+    display: inline-block;
   }
 </style>
