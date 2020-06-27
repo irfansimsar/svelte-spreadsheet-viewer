d2613a69a9df0f9a67eb762c95f15b607d4b53ab
irfan simsar <isimsar@gmail.com>
2020-06-28 00:55:42 +0300

    Fix Pagination updating problem

--- src/components/UI/Pagination.svelte
+++ src/components/UI/Pagination.svelte
@@ -1,14 +1,19 @@
 <script>
+  import { afterUpdate } from 'svelte';
   export let current = 1;
   export let pageSize = 20;
   export let total = 20;
   export let onChange = () => {};
+  let pages = [...Array(Math.ceil(total / pageSize)).keys()];
 
-  const pages = [...Array(Math.ceil(total / pageSize)).keys()];
   const handleOnChange = (page) => {
     current = page;
     onChange(page);
   };
+
+  afterUpdate(() => {
+    pages = [...Array(Math.ceil(total / pageSize)).keys()];
+  });
 </script>
 
 <div class="pagination">
